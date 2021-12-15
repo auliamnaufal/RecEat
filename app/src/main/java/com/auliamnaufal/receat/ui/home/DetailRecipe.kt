59f2bd66e1869e0f8b427f870b6cc79ff147b869
SyauqiAlifI <vstudio776@gmail.com>
2021-12-15 21:36:50 +0700

    details

--- app/src/main/java/com/auliamnaufal/receat/ui/home/DetailRecipe.kt
+++ app/src/main/java/com/auliamnaufal/receat/ui/home/DetailRecipe.kt
@@ -3,8 +3,19 @@ package com.auliamnaufal.receat.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.auliamnaufal.receat.R
+import com.auliamnaufal.receat.databinding.ActivityDetailRecipeBinding
 
 class DetailRecipe : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailRecipeBinding
+
+    companion object {
+        const val TITLE_RECIPE_DETAIL = "title"
+        const val PHOTO_RECIPE_DETAIL = "photo"
+        const val TIME_COOKING_DETAIL = "time"
+        const val RATING_RECIPE_DETAIL = "rate"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_recipe)
