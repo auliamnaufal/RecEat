e24cd673dd3c3ef6a24e357e69ba239dc1f38ac2
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-12-15 20:31:37 +0700

    initial Commit

---
+++ app/src/main/java/com/auliamnaufal/receat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.auliamnaufal.receat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
